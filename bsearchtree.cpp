411e8d3e2ff7e1d76d34b9596fd2c8b721f22a0c
AkilanK <31316900+AkilanK@users.noreply.github.com>
2018-04-02 23:02:30 +0530

    Add files via upload

--- bsearchtree.cpp
+++ bsearchtree.cpp
@@ -62,11 +62,63 @@ void display(node*temp)
 		if(temp==NULL)
 		return;                                                                                                                                                                              
 		display(temp->left);
-		cout<<temp->data;
+		cout<<temp->data<<endl;
 	    display(temp->right);
 	    
 	}
-    
+int search(node*temp,int key)
+{
+if (temp!=NULL)
+{
+search(temp->left,key);
+if(temp->data==key)
+{return key-1;
+}
+search(temp->right,key);
+}
+}
+void deletenode(node*current)
+{
+if(current->left==NULL && current->right==NULL)
+{if(current->parent->left==current)
+  {current->parent->left=NULL;}
+else current->parent->right=NULL;
+    }
+
+else if(current->left==NULL)
+{if(current->parent->left=current)
+   {current->parent->left=current->right;}
+else current->parent->right=current->right;
+}
+else if(current->right==NULL)
+{if(current->parent->left=current)
+   {current->parent->left=current->left;}
+else current->parent->right=current->left;
+}
+
+else{
+node*p;
+p=current->left;
+while(p->right!=NULL)
+{p=p->right;}
+if(p->left==NULL){
+p->parent=current->parent;
+p->left=current->left;
+p->right=current->right;
+{if(current->parent->left=current)
+   {current->parent->left=p;}
+else current->parent->right=p;
+}
+}
+else
+{p->left->parent=p->parent;
+p->parent->right=p->left;
+}
+}
+
+
+
+}
 };
 int main(){
 Btree a;
@@ -77,6 +129,15 @@ a.insert(9);
 a.insert(4);
 a.insert(8);
 a.insert(3);
+a.display(a.root);
+cout<<"Enter the element to search";
+int s;
+cin>>s;
+if(s==a.search(a.root,s)+1)
+{cout<<"the element is found"<<endl;}
+else
+{cout<<"the element is not found"<<endl;}
+a.deletenode(a.root);
 a.display(a.root);
 }
 
